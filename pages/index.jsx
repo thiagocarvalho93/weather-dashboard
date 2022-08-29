aa8fdd6cd8aac150edee4231915cafa84f36aa14
thiagocarvalho93 <thiagopaes1993@gmail.com>
2022-08-29 21:43:10 +0000

    inclusão do highcharts

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,26 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
 export default function Home() {
-  return <div className={styles.container}>
-    <h1>Painel</h1>
-  </div>;
+  const options = {
+    title: {
+      text: "My chart",
+    },
+    series: [
+      {
+        data: [1, 2, 3],
+      },
+    ],
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Painel</h1>
+
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  );
 }
